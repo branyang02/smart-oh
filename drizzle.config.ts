1d072ae872befb2d55c0673d814038214c046895
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2025-02-21 07:44:45 -0500

    add better commands to push to db.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,27 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: ".env.local" });
+let envFile: string;
+
+switch (process.env.PROJECT_ENV) {
+  case "production":
+    envFile = ".env.production";
+    break;
+  case "development":
+    envFile = ".env.development";
+    break;
+  case "preview":
+    envFile = ".env.preview";
+    break;
+  case "test":
+    envFile = ".env.test";
+    break;
+  default:
+    envFile = ".env.development";
+    break;
+}
+
+config({ path: envFile });
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
